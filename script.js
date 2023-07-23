91497d76beccbdb8487a2a9e742e4719508edbc5
N. Omalley Segbee, Jr <116561836+segbeejr@users.noreply.github.com>
2023-07-23 22:57:05 +0000

    Another Commit

--- script.js
+++ script.js
@@ -18,7 +18,6 @@ let sorryElement = document.querySelector(".sorry");
 let try_again = document.querySelector(".try_again");
 let gradeEl = document.querySelector(".grade");
 
-
 let cQuestionIndex = 0;
 let score = 0;
 let grade = 0;
@@ -31,61 +30,39 @@ let percentComplete = 0;
 let click = 0;
 
 
-function restartGame() {
-  updateTimer();
+let timerInterval; 
 
-  cQuestionIndex = 0;
-  score = 0;
-  grade = 0;
-  numCorrect = 0;
-  numWrong = 0;
-  percentComplete = 0;
+start_btn.addEventListener("click", () => {
+  question.style.display = 'block';
+  starting.style.display = 'none';
 
-  scoreEl.innerHTML = '0%';
-  gradeEl.innerHTML = '0%';
-  correct.innerHTML = '0';
-  wrongQuestions.innerHTML = '0';
-  timer.innerHTML = '03:00';
-  Qtext.innerHTML = '';
-  options.innerHTML = '';
-  result_panel.style.display = 'none';
-  sorryElement.style.display = 'none';
-  starting.style.display = 'block';
+  clearInterval(timerInterval);
 
-  questions.forEach(question => {
-    question.userAnswer = undefined;
-  });
-
-  displayQuestion();
-}
-
-
-start_btn.addEventListener("click",()=>{
-
-  question.style.display= 'block';
-  starting.style.display= 'none';
-  
-  function updateTimer(){
+  function updateTimer() {
     let secondsRemaining = 3 * 60;
-    let time = setInterval(()=>{
+    timerInterval = setInterval(() => {
       let minutes = Math.floor(secondsRemaining / 60);
       let seconds = secondsRemaining % 60;
 
-      timer.innerHTML =`${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
-      if(secondsRemaining === 0){
-        clearInterval(time);
+      timer.innerHTML = `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+
+      if (secondsRemaining <= 0) {
+        clearInterval(timerInterval);
         question.style.display = "none";
-        sorryElement.style.display = "block";
-        result_panel.style.display= 'none';
+        result_panel.style.display = 'none';
+        sorryElement.style.display = "block"; 
         restartGame();
-      }
-      else {
+      } else {
         secondsRemaining--;
       }
-    }, 1000)
+    }, 1000);
   }
   updateTimer();
-})
+});
+
+
+
+
 
 show_result.addEventListener("click", ()=>{
   question.style.display= 'none';
@@ -97,12 +74,14 @@ replay.addEventListener("click", ()=>{
   starting.style.display= 'block';
   result_panel.style.display= 'none';
   restartGame();
+  updateTimer();
 })
 
 try_again.addEventListener("click", ()=>{
   starting.style.display= 'block';
   sorryElement.style.display = 'none';
   restartGame();
+  updateTimer();
 })
 
 const questions = [
@@ -244,48 +223,64 @@ const questions = [
 ]
 
 
-function displayQuestion(){
-    const question_data = questions[cQuestionIndex];
-    Qtext.innerHTML = question_data.question;
+function displayQuestion() {
+  const question_data = questions[cQuestionIndex];
+  Qtext.innerHTML = question_data.question;
 
-    const hasAnswered = typeof questions[cQuestionIndex].userAnswer !== 'undefined';
+  const hasAnswered = typeof questions[cQuestionIndex].userAnswer !== 'undefined';
 
-    let opt = "";
+  let opt = "";
 
-    for(let i = 0; i<question_data.options.length; i++){
-      const isDisabled = hasAnswered ? 'disabled' : '';
-      opt += `<li onclick="checkAnswer(event)">${question_data.options[i]}</li>`
+  for (let i = 0; i < question_data.options.length; i++) {
+    const isDisabled = hasAnswered ? 'disabled' : '';
+    opt += `<li onclick="checkAnswer(event)" class="${hasAnswered ? '' : 'selectable'}">${question_data.options[i]}</li>`;
+  }
+
+  options.innerHTML = opt;
+
+  if (hasAnswered) {
+    const correctAnswerIndex = question_data.answer;
+    const selectedOptionIndex = question_data.options.indexOf(question_data.userAnswer);
+
+    const optionsList = options.querySelectorAll('li');
+    optionsList[correctAnswerIndex].classList.add('selected-green');
+    if (selectedOptionIndex !== correctAnswerIndex) {
+      optionsList[selectedOptionIndex].classList.add('selected-red');
     }
+  }
 
-    options.innerHTML = opt;
-
-    updatePercentComplete();
-    updateTotalQuestions();
+  updatePercentComplete();
+  updateTotalQuestions();
 }
 displayQuestion();
 
-function checkAnswer(e){
+function checkAnswer(e) {
   if (typeof questions[cQuestionIndex].userAnswer === 'undefined') {
     let user_answer = e.target.innerHTML;
     let correctAnswer = questions[cQuestionIndex].options[questions[cQuestionIndex].answer];
 
-    
-  if(user_answer==correctAnswer){
-    correct.style.color = "green";
-    score += 5;
-    grade += 5;
-    numCorrect++;
-    correct.innerHTML  = numCorrect;
-    scoreEl.innerHTML = `${score}%`;
-    gradeEl.innerHTML = `${grade}%`;
-  }
-  else if(user_answer != correctAnswer){
-    wrongQuestions.style.color = "red";
-    numWrong++;
-    wrongQuestions.innerHTML = numWrong;
-  }
+    if (user_answer == correctAnswer) {
+      correct.style.color = "green";
+      score += 5;
+      grade += 5;
+      numCorrect++;
+      correct.innerHTML = numCorrect;
+      scoreEl.innerHTML = `${score}%`;
+      gradeEl.innerHTML = `${grade}%`;
+      e.target.classList.add('selected-green');
+    } else {
+      wrongQuestions.style.color = "red";
+      numWrong++;
+      wrongQuestions.innerHTML = numWrong;
+      e.target.classList.add('selected-red');
+    }
 
     questions[cQuestionIndex].userAnswer = user_answer;
+
+    const optionsList = options.querySelectorAll('li');
+    optionsList.forEach(option => {
+      option.classList.remove('selectable');
+    });
   }
 }
 
@@ -325,3 +320,28 @@ function updateTotalQuestions() {
   number_of_question.innerHTML = questions.length - cQuestionIndex - 1;
 }
 
+function restartGame() {
+  cQuestionIndex = 0;
+  score = 0;
+  grade = 0;
+  numCorrect = 0;
+  numWrong = 0;
+  percentComplete = 0;
+
+  scoreEl.innerHTML = '0%';
+  gradeEl.innerHTML = '0%';
+  correct.innerHTML = '0';
+  wrongQuestions.innerHTML = '0';
+  timer.innerHTML = '03:00';
+  Qtext.innerHTML = '';
+  options.innerHTML = '';
+  result_panel.style.display = 'none';
+  sorryElement.style.display = 'none';
+  starting.style.display = 'block';
+
+  questions.forEach(question => {
+    question.userAnswer = undefined;
+  });
+
+  displayQuestion();
+}
